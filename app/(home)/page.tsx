c8632663d5ed120bd3578de9179fde286bc05e8e
psoyeongg <psoyeongg@gmail.com>
2024-02-08 16:12:03 +0900

    client component에서 데이터 fetch

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,22 @@
-export const metadata = {
-  title: "Home",
-};
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Page() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
-  );
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getMovies = async () => {
+    // 브라우저가 요청을 보내므로 클라이언트 컴포넌트에서만 호출 가능
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
 }
