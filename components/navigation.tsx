a57e6c9187fc7c49d3606c97e802fb1f37515557
psoyeongg <psoyeongg@gmail.com>
2024-02-13 16:19:22 +0900

    feat: add CSS Modules and Movie Styles

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,12 +2,13 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
   // usePathname은 client component에서만 작동함
   const path = usePathname();
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <Link href='/'>Home</Link> {path === "/" ? "🔥" : ""}
